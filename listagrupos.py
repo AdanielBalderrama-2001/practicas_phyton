f68456443e5079d5b54a38a6889aa922b8b6514a
AdanielBalderrama-2001 <danielbalderrama2001@gmail.com>
2025-06-11 09:45:12 -0400

    Add files via upload

---
+++ listagrupos.py
@@ -0,0 +1,5 @@
+# 1. Crear una lista con nombres de estudiantes
+Death_Service  = ['Adaniel_Balderrama', 'Carlos_Suarez', 'Marco_Barriga']
+# 2 y 3. Usar bucle for para recorrer la lista e imprimir mensaje personalizado
+for nombre in Death_Service:
+ print(f"¡Bienvenido al equipo Death Services, {nombre}!")
