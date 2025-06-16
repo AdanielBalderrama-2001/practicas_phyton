fbcb63670d302eff581dbf1ae48f309fc298847e
AdanielBalderrama-2001 <danielbalderrama2001@gmail.com>
2025-06-16 10:00:46 -0400

    Add files via upload

---
+++ lista_De_comidas.py
@@ -0,0 +1,7 @@
+Lista_de_comida_favoritas = ['Charquecan', 'Pique macho', 'Sopa de mani','Silpancho']
+print(Lista_de_comida_favoritas)
+Mi_comida_favorita = Lista_de_comida_favoritas [0]
+print(f"Mi comida favorita es: {Mi_comida_favorita}")
+
+
+print(f"Adaniel Balderrama-FIN DEL PROGRAMA")
