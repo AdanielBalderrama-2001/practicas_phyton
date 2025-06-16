c6df532382ca16591f43afc5fb53f95fb565345a
AdanielBalderrama-2001 <danielbalderrama2001@gmail.com>
2025-06-16 18:02:09 -0400

    Add files via upload

---
+++ vector misterioso.py
@@ -0,0 +1,15 @@
+# Lista secreta (vector misterioso)
+vector_misterioso = [3, 7, 2, 9, 5]  # Puedes cambiar los números si deseas
+
+# Puedes hacer preguntas como estas:
+print(len(vector_misterioso))         # ¿Cuántos elementos tiene la lista?
+print(vector_misterioso[0])           # ¿Cuál es el primer número?
+print(vector_misterioso[2])           # ¿Cuál es el tercer número?
+print(vector_misterioso[-1])          # ¿Cuál es el último número?
+print(vector_misterioso[0] > 10)      # ¿El primer número es mayor a 10?
+print(sum(vector_misterioso))         # ¿Cuál es la suma de todos los números?
+
+# BONUS: Recorre la lista y muestra todos los números (una vez descubiertos)
+for numero in vector_misterioso:
+    print(numero)
+print(f"Adaniel Balderrama-FIN DEL PROGRAMA")
