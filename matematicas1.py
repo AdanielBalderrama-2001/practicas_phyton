e5e4a2f5ec882b7b1af5fcc45340749fbe7ee765
AdanielBalderrama-2001 <danielbalderrama2001@gmail.com>
2025-06-16 10:01:41 -0400

    Add files via upload

---
+++ matematicas1.py
@@ -0,0 +1,12 @@
+acumulador_suma = 0
+lista = [5,2,8]
+acumulador_suma = 0 + 5
+acumulador_suma = 5 + 2
+acumulador_suma = 7 + 8
+print(f"El valor total de la suma es: {acumulador_suma}")
+
+lista_numero = [5,2,8]
+acumulador_suma = 0
+for numero in lista_numero:
+    acumulador_suma += numero
+print(f"La suma total es : {acumulador_suma}")
